8a576d6946048ca1fa822f15652b4bb309d9f333
Shiva Ragannagari <ragannagarishiva@gmail.com>
2026-01-01 18:12:05 +0530

    Add files via upload

---
+++ Pog26.js
@@ -0,0 +1,28 @@
+/*Windows Objects to Print the values
+console.log(window.alert);
+console.log(window.document);
+console.log(window.frameElement);
+console.log(window.history);
+console.log(window.length);
+console.log(window.localStorage);
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+console.log(window.opener);
+console.log(window.outerHeight);
+console.log(window.outerWidth);
+console.log(window.name);
+console.log(window.parent);
+console.log(window.sessionStorage);
+console.log(window.scrollX);
+console.log(window.scrollY);
+console.log(window.self);
+console.log(window.top);
+console.log(window.screen);
+console.log(window.history);
+console.log(window.pageXOffset);
+console.log(window.pageYOffset);
+console.log(window.screenLeft);
+console.log(window.screenTop);
+console.log(window.screenX);
+console.log(window.screenY);
+console.log(window.navigator); */
---
+++ Pog27.js
@@ -0,0 +1,18 @@
+//window obj
+mac.alert("This ragannagari");
+console.log(mac.innerWidth);
+//navigator
+console.log(navigator.userAgent);
+console.log(navigator.language);
+//location
+console.log(location.href);
+location.href="https://youtu.be/lKjxgXhFS3A?si=rirF8l4beXuBeqs1";
+//screen
+console.log(screen.width);
+console.log(screen.height);
+//history
+history.back();
+history.forward();
+//size
+let userWindow=window.open("https://youtu.be/lKjxgXhFS3A?si=rirF8l4beXuBeqs1","width=500,heigth=400");
+newWindow.resizeTo(250,250);
